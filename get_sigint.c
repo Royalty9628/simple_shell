34d2a3af9a20de76c7f6cab4f9cbab98c4642b12
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-08-29 14:58:36 -0700

    prompt call

---
+++ get_sigint.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * git_sigint - Handle the ctrl + ccall in prompt
+ * @sig: signal handler
+ * Return: always 0
+ */
+
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^", 5);
+}
